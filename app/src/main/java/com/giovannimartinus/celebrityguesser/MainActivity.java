3809e95cb81285015021f4b336cdc8661163bae5
Giovanni21M <giovanni21martinus@gmail.com>
2019-01-13 14:36:43 -0500

    Create a RelativeLayout w/ Buttons and ImageView

--- app/src/main/java/com/giovannimartinus/celebrityguesser/MainActivity.java
+++ app/src/main/java/com/giovannimartinus/celebrityguesser/MainActivity.java
@@ -3,10 +3,18 @@ package com.giovannimartinus.celebrityguesser;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     final CelebGuess celebGuess = new CelebGuess();
+
+    Button answerOne;
+    Button answerTwo;
+    Button answerThree;
+    Button answerFour;
+    ImageView celebImageView;
 
     class CelebGuess {
 
@@ -22,5 +30,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        answerOne = (Button) findViewById(R.id.answerOne);
+        answerTwo = (Button) findViewById(R.id.answerTwo);
+        answerThree = (Button) findViewById(R.id.answerThree);
+        answerFour = (Button) findViewById(R.id.answerFour);
+        celebImageView = (ImageView) findViewById(R.id.celebImageView);
     }
 }
